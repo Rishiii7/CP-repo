12957908e7d6bfc16fa2e8501b4cb9e6beeea7df
= <7rishi20ss@gmail.com>
2022-12-21 18:38:17 -0700

    power set commit

--- Competitive coding/Bitwise/Power_set.cpp
+++ Competitive coding/Bitwise/Power_set.cpp
@@ -11,11 +11,12 @@ void PowerSet(string s)
 {
     int n = s.length();
     int p_size = 1 << n;
-    cout<<" "<<endl;
     for(int i = 0 ; i < p_size ; i++)
     {
         for(int j = 0 ; j < n ; j ++)
         {
+            // checking whether i_th bit is set or not, if set print the character
+            // 1 << j represent left shift last bit by j number
             if ( (i & (1 << j) ) != 0)
             {
                 cout<<s[j];
