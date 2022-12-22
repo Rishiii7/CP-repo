513d27c9d6cac11aa25e490d6b0c074fe5407186
= <7rishi20ss@gmail.com>
2022-12-21 18:44:43 -0700

    Update odd_two_occuring.cpp

--- Competitive coding/Bitwise/odd_two_occuring.cpp
+++ Competitive coding/Bitwise/odd_two_occuring.cpp
@@ -13,7 +13,10 @@ void TwoOddOcurring(vector <int> arr)
     {
         res = res ^ arr[i];
     }
-
+    
+    //k here is the last set bit of the res varivable
+    // therefore array get divide into two one where last set bit of res is in number
+    // and other where the last set bit of the number is not present
     int k = res & (~ (res - 1));
     int res1 = 0;
     int res2 = 0;
