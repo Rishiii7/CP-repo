b107e7405d6b40938bc1b7de0760e8317d6d0e05
= <7rishi20ss@gmail.com>
2023-02-11 10:07:23 -0700

    added lomuto partition alg

--- Competitive coding/Sort/quick_sort.cpp
+++ Competitive coding/Sort/quick_sort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 /*
 Problem : 
@@ -9,7 +10,45 @@ Different Types of Partition method :
 1. Naive Partition ==>
     Time complexity :- O(n)
     Aux Space complrxity :- O(n)
+    Require 3 O(n) traversal
+
+2. Lomuto Partition ==>
+    Time commplexity :- O(n)
+    Aux Space complexity :- O(1)
+    Require 1 O(n) traversal
+ 
 */
+
+int LomutoPartition(vector <int> &arr, int l, int h, int pivot)
+{
+    /*
+    Args ::
+    arr : Integer Vector
+    l ,h : Integer -- indices of array where sorting is performed
+    pivot : Integer -- index element which get sorted
+    
+    Parameters ::
+    pivot_ele : Integer -- element of pivot index
+    i : Integer -- indicate the size of element smaller than pivot
+    
+    Returns ::
+    Integer -- index of pivot after sorting
+    */
+    int pivot_ele = arr[pivot];
+    swap(arr[h], pivot_ele);
+    int i = l-1;
+    for (int j = l; j < h; j++)
+    {
+        if (arr[j] < pivot_ele)
+        {
+            i++;
+            swap(arr[j], arr[i]);
+        }
+    }
+
+    swap(arr[i+1], arr[pivot]);
+    return i+1;
+}
 
 
 void NaivePartition(vector <int> &arr, int l , int r , int pivot)
@@ -69,7 +108,15 @@ int main()
     }
 
     // Only partition function
-    NaivePartition(arr , 0 , n-1 , 3);
+    /*NaivePartition(arr , 0 , n-1 , 3);
+    cout<<"Sorted array : ";
+    for(int i = 0 ; i < n ; i++)
+    {
+        cout<<arr[i]<<" ";
+    }*/
+
+    //Lamouto partition function
+    cout<<"Index at pivot : "<<LomutoPartition(arr, 0, n-1, n-1);
     cout<<"Sorted array : ";
     for(int i = 0 ; i < n ; i++)
     {
